4d09b3f09bee7189562a821bf1c66485782cff63
yuan <115965720+sunyuanting@users.noreply.github.com>
2025-05-23 11:41:28 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ const indices = [409,270,269,267,0,37,39,40,185,61,146,91,181,84,17,314,405,321,
 const indices2 = [76,77,90,180,85,16,315,404,320,307,306,408,304,303,302,11,72,73,74,184];
 const leftEyeIndices = [243, 190, 56, 28, 27, 29, 30, 247, 130, 25, 110, 24, 23, 22, 26, 112];
 const rightEyeIndices = [263, 466, 388, 387, 386, 385, 384, 398, 362, 382, 381, 380, 374, 373, 390, 249];
+const foreheadIndices = [151, 109, 69, 66, 55, 8, 285, 296, 299, 338];
 
 function setup() {
   createCanvas(640, 480).position(
@@ -113,5 +114,31 @@ function draw() {
     const [xStartRight, yStartRight] = keypoints[rightEyeIndices[0]];
     const [xEndRight, yEndRight] = keypoints[rightEyeIndices[rightEyeIndices.length - 1]];
     line(xStartRight, yStartRight, xEndRight, yEndRight);
+
+    // 使用 line 指令連接 foreheadIndices 並填滿粉紅色
+    fill(255, 182, 193, 150); // 半透明粉紅色
+    noStroke();
+    beginShape();
+    for (let i = 0; i < foreheadIndices.length; i++) {
+      const idx = foreheadIndices[i];
+      const [x, y] = keypoints[idx];
+      vertex(x, y);
+    }
+    endShape(CLOSE);
+
+    // 繪製額頭連線
+    stroke(255, 0, 0); // 紅色線條
+    strokeWeight(10); // 粗細為 10
+    for (let i = 0; i < foreheadIndices.length - 1; i++) {
+      const idx1 = foreheadIndices[i];
+      const idx2 = foreheadIndices[i + 1];
+      const [x1, y1] = keypoints[idx1];
+      const [x2, y2] = keypoints[idx2];
+      line(x1, y1, x2, y2);
+    }
+    // 將最後一點與第一點連接
+    const [xStartForehead, yStartForehead] = keypoints[foreheadIndices[0]];
+    const [xEndForehead, yEndForehead] = keypoints[foreheadIndices[foreheadIndices.length - 1]];
+    line(xStartForehead, yStartForehead, xEndForehead, yEndForehead);
   }
 }
